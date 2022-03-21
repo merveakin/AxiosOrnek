984416e66da1ab3deb1d060763a4ca7b2caedea5
merveakin <merveakinnnn@hotmail.com>
2022-03-21 09:19:52 +0300

    Axios Ornek

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { ActivityIndicator, StyleSheet, View, Text, SafeAreaView } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props); //C# daki myContexi base göndermek gibi
+
+    this.state = {
+      isLoading: true, //isLoading state içerisinde tutulan bir değişkendir
+      data: [] //state içerisinde adı data olan boş bir dizi tanımlı
+    }
+  }//ctor burada biter
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users/4')
+      .then(
+        (response)=>{
+        return response.data;
+        }
+      )
+      .then((d)=>
+        {
+          alert(d.name)
+        })
+
+      .catch(
+        (error) => {
+          alert(error);
+        }
+      )
+  } //componentDidMount burada biter
+  render() {
+    if (this.state.isLoading) {
+      //isLOading true ise =>
+      return (
+        <SafeAreaView>
+          <View style={styles.container}>
+            <ActivityIndicator />
+          </View>
+        </SafeAreaView>
+      );//return burada biter
+
+    }///if biter
+    else {
+     
+    }//else biter
+
+
+  }//render burada biter
+
+
+}//class burada biter
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'powderblue',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+
   },
-});
+  dataitem: {
+    flex: 1,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+
+}); //stylesheet burada biter
